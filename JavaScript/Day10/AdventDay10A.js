40873d487e5e98d52f28d0d3fb28a35287abed4c
Josh Dovishaw <jdovishaw@xceleratemedia.com>
2015-12-10 12:19:09 -0500

    Day 10 completed first in Ruby, because n.times and again in JS because curious about performance.

---
+++ JavaScript/Day10/AdventDay10A.js
@@ -0,0 +1,15 @@
+(function() {
+	var input = '3113322113', parts;
+	
+	for(var i = 0; i < 40; i++) {
+		var temp = '';
+		parts = input.match(/(\d)\1*/g);
+		parts.forEach(function(s) {
+			temp += s.length + s[0];
+		});
+		input = temp;
+	} 
+	
+	console.log('Output: ' + input);
+	console.log('Length: ' + input.length);
+})();
---
+++ Ruby/Day10/AdventDay10A.rb
@@ -0,0 +1,10 @@
+input = '3113322113'
+
+40.times do
+	input = input.gsub(/(\d)\1*/) { | set |
+		set.length.to_s + set[0]
+	}
+end
+
+puts 'Output: ' + input
+puts 'Length: ' + input.length.to_s
