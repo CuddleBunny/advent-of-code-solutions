6511f7530eee7b3325771177d4822fbb8d0a3126
Josh Dovishaw <cuddlebunny3@live.com>
2015-12-14 15:27:04 -0500

    shortened condition

--- Ruby/Day04/AdventDay4A.rb
+++ Ruby/Day04/AdventDay4A.rb
@@ -3,9 +3,7 @@ require 'digest'
 input = 'bgvyzdsv'; answer = 0
 
 while(true)
-	if Digest::MD5.hexdigest(input + answer.to_s)[0...5] == '00000'
-		break
-	end
+	break if Digest::MD5.hexdigest(input + answer.to_s)[0...5] == '00000'
 	answer += 1
 end
 
